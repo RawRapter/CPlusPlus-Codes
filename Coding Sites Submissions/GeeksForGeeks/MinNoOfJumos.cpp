d75d1d4b9389564240f5f2fdac0428ffd436a633
Anant Arun <anant.arun92@gmail.com>
2022-07-08 19:49:48 +0530

    CSS-GFG: MNOJ add

---
+++ Coding Sites Submissions/GeeksForGeeks/MinNoOfJumos.cpp
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// Function to return minimum number of jumps to end of array
+
+class Solution{
+  public:
+    int minJumps(int arr[], int n){
+        // Your code here
+        if(n<=1)
+            return 0;
+        else if(arr[0]==0)
+            return -1;
+
+        int c=0,r=0;
+        //int i=0;
+        int jump=0;
+        for(int i=0; i<n-1; i++){
+            
+            /*if(arr[i]==0)
+                return jump;*/
+            
+            c = max(c,i+arr[i]);
+            if(i==r){
+                jump++;
+                r=c;
+            }
+                
+                
+        }
+        
+        if(c<n-1)
+            return -1;
+        return jump;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i,j;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        Solution obj;
+        cout<<obj.minJumps(arr, n)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
